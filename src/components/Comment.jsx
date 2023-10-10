5999ede60c489411cbcce7694c62efeb03f414f9
Arlen Agra <arlens527@gmail.com>
2023-10-10 07:17:29 -0300

    --atualizaçoes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,34 @@
-import styles from './Comment.module.css'
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
 
 export function Comment() {
-  return(
-    <div className={styles.comment}></div>
-  )
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/Arlen028.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Arlen Agra</strong>
+              <time dateTime="2023-08-25 as 18:30">Cerca de 1h atrás</time>
+
+              <button title="Deletar comentário">
+                <Trash size={20} />
+              </button>
+            </div>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
